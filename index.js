1e08328f5ea6a6557c218e6e874de5d0deae6810
LEE Seungje <38553239+sonyun247@users.noreply.github.com>
2021-02-21 15:46:16 +0900

    Optional parameter

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-console.log("hello");
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const name = "TS", age = 18, gender = "Male";
+const sayHi = (name, age, gender) => {
+    console.log(`Hi! ${name}, you're ${age}, and ${gender}`);
+};
+sayHi(name, age);
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,1 +1,9 @@
-console.log("hello");
+const name = "TS", age = 18, gender = "Male";
+
+const sayHi = (name, age, gender?) => {
+  console.log(`Hi! ${name}, you're ${age}, and ${gender}`);
+};
+
+sayHi(name, age);
+
+export {};
